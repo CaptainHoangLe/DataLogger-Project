7424b3723e43b166f7345824d79bb154b2ab45e2
Viet Hoang Le <53038465+CaptainHoangLe@users.noreply.github.com>
2021-10-29 00:01:05 +0700

    replace vue3 to vue 2

--- datalogger-frontend/src/App.vue
+++ datalogger-frontend/src/App.vue
@@ -1,23 +1,68 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-app-bar app color="#173D6A" height="60vh">
+      <v-app-bar-nav-icon
+        id="navbarText"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title class="col-md-4" id="navbarText"
+        >Lê Việt Hoàng</v-toolbar-title
+      >
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" fixed color="#173D6A" width="280px" >
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-title>John Leider</v-list-item-title>
+
+        <!-- <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn> -->
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <!-- <HelloWorld/> -->
+    </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
-}
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Tổng quan", icon: "mdi-home-city" },
+        { title: "Thiết bị", icon: "mdi-account" },
+        { title: "Users", icon: "mdi-account-group-outline" },
+      ],
+      mini: true,
+    };
+  },
+};
 </script>
